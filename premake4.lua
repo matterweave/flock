3da92be14a78fdf7ec2fed9980c4b11ed4a43319
Dylan Sarber <dwsarber@gmail.com>
2012-04-12 18:09:45 -0700

    Windows 3rd-party library directories
    Conditional logic has been added to the premake build file to automatically use
    the correct libraries based on the platform and build configuration.
    
    At the moment, an x86 build on windows is all that's really going to be supported,
    but linux support will be in very soon. A few libraries are still not in the project
    folder, so builds are still broken.

--- premake4.lua
+++ premake4.lua
@@ -2,10 +2,31 @@
 
 PROJ_NAME = "Gorgon"
 
+-- Premake OS Constants
+WINDOWS = "windows"
+LINUX = "linux"
+OSX = "macosx"
+
 DEBUG_DEFINES = { "DEBUG" }
 DEBUG_FLAGS = { "Symbols" }
 RELEASE_DEFINES = { "NDEBUG" }
 RELEASE_FLAGS = { "Optimize" }
+
+DEBUG_LIB_DIRS = {}
+RELEASE_LIB_DIRS = {}
+WINDOWS_X86_DEBUG = "lib/debug/nt/x86"
+WINDOWS_X86_RELEASE = "lib/release/nt/x86"
+
+if os.is(WINDOWS) then
+	DEBUG_LIB_DIRS.insert(WINDOWS_X86_DEBUG)
+	RELEASE_LIB_DIRS.insert(WINDOWS_X86_RELEASE)
+end
+
+elseif os.is(OSX) then
+end
+
+elseif os.is(LINUX) then
+end
 
 solution(PROJ_NAME)
 	configurations { "Debug", "Release" }
@@ -17,9 +38,11 @@ solution(PROJ_NAME)
         links { "logog", "ybrec", "SDL" }
 
 		configuration "Debug"
+			libdirs { DEBUG_LIB_DIRS }
 			defines { DEBUG_DEFINES }
 			flags { DEBUG_FLAGS }
 
 		configuration "Release"
+			libdirs { RELEASE_LIB_DIRS }
 			defines { RELEASE_DEFINES }
 			flags { RELEASE_FLAGS }
