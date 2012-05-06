498bc5b0f154de98ee74cb4cd1194b88b29136ae
Dylan Sarber <dwsarber@gmail.com>
2012-05-06 14:10:00 -0700

    Unix libraries and updates
    * A few more ignores for sublime text are now present.
    * The premake file no longer takes into account the ScriptManager class,
      as it's not needed right now.
    * A unix build of Logog is available in the lib folder. Only release
      versions of 3rd-party libs will be used. This is to save space and
      because it isn't really necessary to have debug info on code we didn't
      write.

--- premake4.lua
+++ premake4.lua
@@ -1,29 +1,36 @@
--- Gorgon Engine Solution
+-- Flock Engine Solution
 
-PROJ_NAME = "Gorgon"
+PROJ_NAME = "Flock"
 
 -- Premake OS Constants
 WINDOWS = "windows"
 LINUX = "linux"
 OSX = "macosx"
 
+-- Standard project build settings
 DEBUG_DEFINES = { "DEBUG" }
 DEBUG_FLAGS = { "Symbols" }
 RELEASE_DEFINES = { "NDEBUG" }
 RELEASE_FLAGS = { "Optimize" }
 
+-- Constants for included libraries
 DEBUG_LIB_DIRS = {}
 RELEASE_LIB_DIRS = {}
 WINDOWS_X86_DEBUG = "lib/debug/nt/x86"
 WINDOWS_X86_RELEASE = "lib/release/nt/x86"
+UNIX_RELEASE = "lib/release/unix"
 
+-- Set up included libs
 if os.is(WINDOWS) then
     table.insert(DEBUG_LIB_DIRS, WINDOWS_X86_DEBUG)
     table.insert(RELEASE_LIB_DIRS, WINDOWS_X86_RELEASE)
 elseif os.is(OSX) then
 elseif os.is(LINUX) then
+    table.insert(DEBUG_LIB_DIRS, UNIX_RELEASE)
+    table.insert(RELEASE_LIB_DIRS, UNIX_RELEASE)
 end
 
+-- Define the project
 solution(PROJ_NAME)
     configurations { "Debug", "Release" }
 
@@ -31,6 +38,7 @@ solution(PROJ_NAME)
         kind("ConsoleApp")
         language("C++")
         files { "src/**.h", "src/**.cpp" }
+        excludes { "src/scriptmanager.*" }
         links { "logog", "SDL" }
         includedirs { "include" }
 
