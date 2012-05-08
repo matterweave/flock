7ba20db88e7f18a0412990577ef21388b19ff325
Dylan Sarber <dwsarber@gmail.com>
2012-05-08 15:58:10 -0700

    Mesh2D implementation
    Basic loading of an image from a given path is implemented. The surface
    used for SDL is automatically freed upon destruction.

---
+++ src/mesh2d.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2012, Dylan Sarber <dwsarber@gmail.com>
+ *
+ * See LICENSE for licensing information.
+ */
+
+#include "mesh2d.h"
+
+#include "logog/logog.hpp"
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+
+/* Constructors & Destructor
+------------------------------------------------------------------------------*/
+
+Mesh2D::Mesh2D(std::string path) {
+    data = IMG_Load(path.c_str());
+
+    if (data == NULL)
+        ERR("Failed to initialize mesh at: %s", path.c_str());
+}
+
+Mesh2D::~Mesh2D() {
+    if (data != NULL)
+        SDL_FreeSurface(data);
+}
