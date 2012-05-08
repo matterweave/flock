566bc196158377eacafd6875f2ce0b367c0117f0
Dylan Sarber <dwsarber@gmail.com>
2012-05-07 23:01:17 -0700

    GameObject Implementation Stubs
    * Add gameobject.cpp with blank cons/dest.

---
+++ src/gameobject.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2012, Dylan Sarber <dwsarber@gmail.com>
+ *
+ * See LICENSE for licensing information.
+ */
+
+#include "gameobject.h"
+
+/* Constructors & Destructor
+------------------------------------------------------------------------------*/
+
+GameObject::GameObject() {}
+
+GameObject::~GameObject() {}
