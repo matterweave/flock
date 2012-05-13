9072a489237522db1f3319f1afdab99ab31bda82
Dylan Sarber <dwsarber@gmail.com>
2012-05-12 19:17:28 -0700

    Add initial implementation of Component base class.

---
+++ src/component.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2012, Dylan Sarber <dwsarber@gmail.com>
+ *
+ * See LICENSE for licensing information.
+ */
+
+#include "component.h"
+#include "gameobject.h"
+
+/* Constructors & Destructor
+------------------------------------------------------------------------------*/
+
+Component::Component(const char *name, const GameObject &parent)
+    : name(name), parent(parent)
+{}
+
+Component::~Component() {}
---
+++ src/component.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2012, Dylan Sarber <dwsarber@gmail.com>
+ *
+ * See LICENSE for licensing information.
+ */
+
+#ifndef _COMPONENT_H_
+#define _COMPONENT_H_
+
+class GameObject;
+
+/**
+ * The base class for any extension of objects in the game.
+ *
+ * Some components have very specialized functionality, whereas scripts are fairly
+ * general in what they can do. At the very least, each component has something that it
+ * does during a simulation update and a name for identification purposes. The name
+ * should be set by the implementing class.
+ */
+class Component {
+public:
+    /**
+     * Construct a component with a name and parent.
+     *
+     * @param name The human-readable name of the component. Should be consistent for all
+     * components of this type (i.e. it should be "transform" for every Transform component).
+     * @param parent The GameObject this component is attached to.
+     */
+    explicit Component(const char *name, const GameObject &parent);
+
+    virtual ~Component();
+
+    /**
+     * Update the state of the Component in the simulation.
+     *
+     * Each Component implementation will handle this differently.
+     */
+    virtual void update() = 0;
+
+private:
+    const char *name;
+    GameObject &parent;
+
+    Component(const Component &copy);
+    Component & operator=(const Component &rhs);
+};
+
+#endif // _COMPONENT_H_
