8ffab05c3bcab4bac7a6c0851167c27b3687c3cb
Dylan Sarber <dwsarber@gmail.com>
2012-05-13 23:10:24 -0700

    Preliminary rendering logic
    Extremely primitive rendering logic is in place. Layers are not yet
    taken into account, and things are rendered in order of draw calls.
    
    Formatting and documentation in the Renderer files has been cleaned up
    to match the rest of the code.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -5,6 +5,8 @@
  */
 
 #include "renderer.h"
+#include "sprite.h"
+#include "transform.h"
 #ifdef WINDOWS
 #include "SDL.h"
 #else
@@ -14,14 +16,30 @@
 /* Constructors & Destructor
 ------------------------------------------------------------------------------*/
 
-Renderer::Renderer() : height(600), width(800), bitDepth(32)
-{
+Renderer::Renderer() : height(600), width(800), bitDepth(32) {
     SDL_Init(SDL_INIT_EVERYTHING);
     canvas = SDL_SetVideoMode(width, height, bitDepth, SDL_HWSURFACE);
 }
 
-Renderer::~Renderer()
-{
+Renderer::~Renderer() {
     SDL_Quit();
 }
 
+/* Public Interface
+------------------------------------------------------------------------------*/
+
+void Renderer::draw(const Transform &pos, const Sprite &sprite) {
+    SDL_Rect dest;
+    Mesh2D mesh = sprite.getMesh();
+
+    dest.x = pos.getX();
+    dest.y = pos.getY();
+    dest.w = mesh.getWidth();
+    dest.h = mesh.getHeight();
+
+    SDL_BlitSurface(mesh.data, NULL, canvas, &dest);
+}
+
+void Renderer::display() {
+    SDL_Flip(canvas);
+}
